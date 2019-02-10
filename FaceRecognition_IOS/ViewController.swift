dde85c38bb3b116a5ea728e395c2dd40bc285267
Onurcan Yurt <onurcanyurt@gmail.com>
2019-02-11 02:06:03 +0300

    Update ViewController.swift

--- FaceRecognition_IOS/ViewController.swift
+++ FaceRecognition_IOS/ViewController.swift
@@ -7,20 +7,15 @@ import UIKit
 import AVKit
 import Vision
 
-//Machine Learning kısmına gecelim oncelikle vision u import ettik
 
 class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
-    //AVCaptureVideoDataOutputSampleBufferDelegate i sinifimizi dahil ettik
 
     @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //info.plist ten izin almayı unutmuyoruz(camera usage)
-        
-        //Burada öncelikle kamerayı olusturdugumuz kodları yazıyoruz
         let captureSession = AVCaptureSession()
-        captureSession.sessionPreset = .photo //ekranda gorulecek kameranın boyutu
+        captureSession.sessionPreset = .photo 
         
         guard let captureDevice = AVCaptureDevice.default(for: .video) else {
             return
@@ -46,25 +41,23 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         captureSession.addOutput(dataOutput)
     }
     
-    //Capture video output ile ilgili bi metodumuzu override ettik
+    
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         
         guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
             return
         }
         
-        //projemize dahil ettigimiz Resnet50 modelimizi burada tanımladık ve requeste ekledik
+        
         guard let model = try? VNCoreMLModel(for: people().model) else{ return }
         let request = VNCoreMLRequest(model: model) { (finishedReq, err) in
             
-            
-            //burada sonuclari yazdırıyoruz
-            //tum sonuclara results degiskeni ile eristik
+                                                     
             guard let results = finishedReq.results as? [VNClassificationObservation] else { return }
             guard let firstObservation = results.first else { return }
-            //firstObservation ile en kesin sonuca eristik
+                                                     
             
-            //ve label e yazdırdık
+                                                     
             DispatchQueue.main.async {
                 
                 self.label.text = " \(firstObservation.identifier) \(firstObservation.confidence)"
