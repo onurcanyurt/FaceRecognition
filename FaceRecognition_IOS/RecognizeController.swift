73966a5b268f54651f92a331be0a5876c4746450
Onurcan Yurt <onurcanyurt@gmail.com>
2020-11-22 18:29:42 +0300

    Refactor RecognizeController

--- FaceRecognition_IOS/RecognizeController.swift
+++ FaceRecognition_IOS/RecognizeController.swift
@@ -7,69 +7,46 @@ import UIKit
 import AVKit
 import Vision
 
+class RecognizeController: UIViewController {
 
-class RecognizeController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
-
-    @IBOutlet weak var label: UILabel!
+    @IBOutlet private weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configureCapture()
+    }
+    
+    private func configureCapture() {
         let captureSession = AVCaptureSession()
         captureSession.sessionPreset = .photo 
         
-        guard let captureDevice = AVCaptureDevice.default(for: .video) else {
-            return
-        }
-        
-        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else{
-            
-            return
-        }
-        
+        guard let captureDevice = AVCaptureDevice.default(for: .video), let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
         captureSession.addInput(input)
         captureSession.startRunning()
         
         let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        
         view.layer.addSublayer(previewLayer)
-        
         previewLayer.frame = view.frame
-        
         
         let dataOutput = AVCaptureVideoDataOutput()
         dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
         captureSession.addOutput(dataOutput)
     }
-    
-    
+}
+
+extension RecognizeController: AVCaptureVideoDataOutputSampleBufferDelegate {
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer),
+              let model = try? VNCoreMLModel(for: people().model) else { return }
         
-        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
-            return
-        }
-        
-        
-        guard let model = try? VNCoreMLModel(for: people().model) else{ return }
         let request = VNCoreMLRequest(model: model) { (finishedReq, err) in
-            
-                                                     
-            guard let results = finishedReq.results as? [VNClassificationObservation] else { return }
-            guard let firstObservation = results.first else { return }
-                                                     
-            
-                                                     
+            guard let results = finishedReq.results as? [VNClassificationObservation], let firstObservation = results.first else { return }
             DispatchQueue.main.async {
-                
                 self.label.text = " \(firstObservation.identifier) \(firstObservation.confidence)"
             }
-            
         }
         
         try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
-        
     }
-
-
 }
 
 
